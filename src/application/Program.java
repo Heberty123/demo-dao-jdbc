71b4933ae8190c565575e4a40cbf17bdfab56c72
Heberty <hebertyprado22005@gmail.com>
2022-07-06 19:28:16 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import Model.entities.Department;
+import Model.entities.Seller;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
@@ -8,13 +9,15 @@ import db.DbIntegrityException;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] argc){
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
 
     }
 }
