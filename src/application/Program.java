09523a0fa1ea40ee34a20204036f3acc29e7200c
Heberty <hebertyprado22005@gmail.com>
2022-07-06 19:46:12 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import Model.dao.DaoFactory;
+import Model.dao.SellerDao;
 import Model.entities.Department;
 import Model.entities.Seller;
 import db.DB;
@@ -16,8 +18,11 @@ public class Program {
     public static void main(String[] argc){
 
         Department obj = new Department(1, "books");
+        
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println(seller);
     }
 }
