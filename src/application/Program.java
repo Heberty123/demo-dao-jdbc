8176f3b340ba0c24156e6c0c943289c3f6d51f3b
Heberty <hebertyprado22005@gmail.com>
2022-07-07 14:53:58 -0300

    findBtDepartment implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,11 +7,11 @@ import Model.entities.Seller;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
-
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -21,8 +21,14 @@ public class Program {
 
         System.out.println("=== TEST 1: Seller findById =====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: Seller findByDepartment =====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
 
     }
 }
