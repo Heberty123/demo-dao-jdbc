253da9805b1e8969553b51845cfb91bdfde48658
Heberty <hebertyprado22005@gmail.com>
2022-07-07 14:18:54 -0300

    att

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,12 +17,11 @@ public class Program {
 
     public static void main(String[] argc){
 
-        Department obj = new Department(1, "books");
-        
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
+
     }
 }
